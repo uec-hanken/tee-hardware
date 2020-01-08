238d90d30d519acca0883cf85e9cade41877ed54
Ckristian Duran <ckdur.iso@gmail.com>
2020-01-08 14:51:54 +0900

    Xilinx filed added

---
+++ fpga/vc707/keystone-NEDOFPGA/keystone-NEDOFPGA.srcs/sources_1/ip/pll/pll_stub.vhdl
@@ -0,0 +1,33 @@
+-- Copyright 1986-2016 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2016.4 (lin64) Build 1756540 Mon Jan 23 19:11:19 MST 2017
+-- Date        : Tue Jan  7 10:07:21 2020
+-- Host        : ckdur-VirtualBox running 64-bit Ubuntu 18.04.3 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/ckdur/Documents/keystone-NEDOFPGA/keystone-NEDOFPGA.srcs/sources_1/ip/pll/pll_stub.vhdl
+-- Design      : pll
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7vx485tffg1761-2
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity pll is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    clk_out2 : out STD_LOGIC;
+    clk_out3 : out STD_LOGIC;
+    reset : in STD_LOGIC;
+    locked : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end pll;
+
+architecture stub of pll is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,clk_out2,clk_out3,reset,locked,clk_in1";
+begin
+end;
