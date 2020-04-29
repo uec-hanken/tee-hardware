ccf6327e43274d5f83e9ba4d332eb8df49ea62c0
thuchoang90 <thuc@vlsilab.ee.uec.ac.jp>
2020-04-28 23:09:15 -0700

    correct update scripts

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 git submodule update --init
-cd hardware/hardware/chipyard/scripts/
+cd hardware/chipyard/scripts/
 ./init-submodules-no-riscv-tools.sh
 cd ../../../software/freedom-u540-c000-bootloader
 git submodule update --init
--- update_nosim.sh
+++ update_nosim.sh
@@ -1,5 +1,5 @@
 git submodule update --init
-cd hardware/hardware/chipyard/scripts/
+cd hardware/chipyard/scripts/
 ./init-submodules-no-riscv-tools.sh --no-firesim
 cd ../../../software/freedom-u540-c000-bootloader
 git submodule update --init
