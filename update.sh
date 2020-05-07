42047cdb6cf4b8514d1d7811586120123bca4da8
thuchoang90 <thuc@vlsilab.ee.uec.ac.jp>
2020-05-07 12:12:30 -0700

    Move the bootrom/ dir to inside the hardware/chipyard/ dir to avoid the confusion at the top-level folder

--- update.sh
+++ update.sh
@@ -3,4 +3,6 @@ cd hardware/chipyard/scripts/
 ./init-submodules-no-riscv-tools.sh
 cd ../../../software/freedom-u540-c000-bootloader
 git submodule update --init
+cd ../../hardware/chipyard/
+patch -p1 < ../../patches/chipyard.patch
 cd ../../
--- update_nosim.sh
+++ update_nosim.sh
@@ -3,4 +3,6 @@ cd hardware/chipyard/scripts/
 ./init-submodules-no-riscv-tools.sh --no-firesim
 cd ../../../software/freedom-u540-c000-bootloader
 git submodule update --init
+cd ../../hardware/chipyard/
+patch -p1 < ../../patches/chipyard.patch
 cd ../../
