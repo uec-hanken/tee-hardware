19f1d8aac9651031fbaa807054754dd5f80cc89b
thuchoang90 <thuc@vlsilab.ee.uec.ac.jp>
2020-05-11 16:56:05 -0700

    Rename & re-structure the fpga folder

---
+++ fpga/Xilinx/VC707/VC707.srcs/sources_1/ip/pll/pll_stub.v
@@ -0,0 +1,25 @@
+// Copyright 1986-2016 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2016.4 (lin64) Build 1756540 Mon Jan 23 19:11:19 MST 2017
+// Date        : Mon May 11 06:46:33 2020
+// Host        : ubuntu running 64-bit Ubuntu 18.04.4 LTS
+// Command     : write_verilog -force -mode synth_stub
+//               /home/ubuntu/Projects/TEE-HW/tee-hardware/fpga/Xilinx/VC707/VC707.srcs/sources_1/ip/pll/pll_stub.v
+// Design      : pll
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7vx485tffg1761-2
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module pll(clk_out1, clk_out2, clk_out3, reset, locked, 
+  clk_in1)
+/* synthesis syn_black_box black_box_pad_pin="clk_out1,clk_out2,clk_out3,reset,locked,clk_in1" */;
+  output clk_out1;
+  output clk_out2;
+  output clk_out3;
+  input reset;
+  output locked;
+  input clk_in1;
+endmodule
--- fpga/vc707/jumpqspi.mk
+++
@@ -1,9 +0,0 @@
-# ROM Generation for jumping to the qspi
-bootrom_dir=$(base_dir)/software/xip
-
-DTS_FILE = $(build_dir)/$(long_name).dts
-BOOT_FILE = $(build_dir)/xip.hex
-
-$(BOOT_FILE): $(bootrom_dir)/Makefile
-	make -C $(bootrom_dir) BUILD_DIR=$(build_dir) long_name=$(long_name) hex
-
