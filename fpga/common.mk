a8f2468766b969b0a16edb2641020b3193d7a936
Ckristian Duran <ckdur.iso@gmail.com>
2020-06-11 11:52:42 +0900

    rename.ul is optional now.

--- fpga/common.mk
+++ fpga/common.mk
@@ -38,7 +38,7 @@ default: my_verilog
 	# We are going to separate the different tops here
 	cd $(base_dir) && $(SBT) "project $(SBT_PROJECT)" "runMain $(MODEL_PACKAGE).uecutils.MultiTopAndHarness -o $(build_dir)/SHOULDNT.v -i $(FIRRTL_FILE) --syn-tops $(SEPARE) --chip-top $(TOP) --harness-top $(MODEL) -faf $(ANNO_FILE) --infer-rw --repl-seq-mem -c:$(TOP):-o:$(build_dir)/SHOULDNT.mems.conf -td $(build_dir)"
 	#cd $(base_dir) && $(SBT) "project tapeout" "runMain barstools.tapeout.transforms.GenerateTopAndHarness -o $(build_dir)/$(TOP).v -tho $(build_dir)/$(MODEL).v -i $(FIRRTL_FILE) --syn-top $(TOP) --harness-top $(MODEL) -faf $(ANNO_FILE) -tsaof $(build_dir)/$(TOP).anno.json -tdf $(build_dir)/$(TOP).f -tsf $(build_dir)/$(TOP).fir -thaof $(build_dir)/$(MODEL).anno.json -hdf $(build_dir)/$(MODEL).f -thf $(build_dir)/$(MODEL).fir --infer-rw --repl-seq-mem -c:$(MODEL):-o:$(build_dir)/$(TOP).mems.conf -thconf $(build_dir)/$(MODEL).mems.conf -td $(build_dir)"
-	$(shell rename.ul $(MODEL_PACKAGE).$(MODEL).$(CONFIG) $(MODEL) $(build_dir)/*)
+	- rename.ul $(MODEL_PACKAGE).$(MODEL).$(CONFIG) $(MODEL) $(build_dir)/*
 	# We also want to generate our memories
 	cd $(base_dir) && $(SBT) "project $(SBT_PROJECT)" $(MACROCOMPILER_COMMANDS)
 
