06343b5c0d02f629bb78dc94758054e216955dce
roensepe <roensepe@gmail.com>
2021-05-26 14:33:59 +0900

    Poly130 update

---
+++ hardware/teehw/src/main/scala/devices/poly/polyregs.scala
@@ -0,0 +1,27 @@
+package uec.teehardware.devices.poly
+
+
+object PolyRegs {
+  val key_0           = 0x100
+  val key_1           = 0x104
+  val key_2           = 0x108
+  val key_3           = 0x10C
+  val key_4           = 0x110
+  val key_5           = 0x114
+  val key_6           = 0x118
+  val key_7           = 0x11C
+  val block_0         = 0x120
+  val block_1         = 0x124
+  val block_2         = 0x128
+  val block_3         = 0x12C
+  val block_len       = 0x130
+  val mac_0           = 0x134
+  val mac_1           = 0x138
+  val mac_2           = 0x13c
+  val mac_3           = 0x140
+  val init            = 0x144
+  val next            = 0x148
+  val finish          = 0x14c
+  val reset_n         = 0x150
+  val ready           = 0x154
+}
